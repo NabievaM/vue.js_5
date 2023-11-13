8dcc264ee23f9fdc83d0afeb55d8b15086bfd2f3
NabievaM <101929794+NabievaM@users.noreply.github.com>
2023-11-13 21:57:57 +0500

    first commit

---
+++ 2-dars/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+const text = ref("Vue");
+
+const fruits = ref(["Apple", "Pear", "Banana", "Orange"]);
+
+function show_fruits(f) {
+  console.log(f);
+}
+</script>
+
+<template>
+  <div>Hello {{ text }}</div>
+  <div v-for="f of fruits">
+    <div @click="show_fruits(f)">
+      <div>{{ f }}</div>
+    </div>
+  </div>
+  <br /><br />
+
+  <div>
+    <img src="https://picsum.photos/id/1/200/300" alt="" />
+  </div>
+  <button>Randomize image</button>
+</template>
