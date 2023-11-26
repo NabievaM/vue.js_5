0433fc9c078a409a24100a98f86949741598d1de
NabievaM <101929794+NabievaM@users.noreply.github.com>
2023-11-27 04:58:16 +0500

    homework

---
+++ 12-dars/src/components/base-select.vue
@@ -0,0 +1,17 @@
+<template>
+  <select
+    class="px-2"
+    @input="$event => $emit('update.modelValue', $event.target.value)"
+  >
+    <option value="null" disabled>Limit</option>
+    <option value="5">5</option>
+    <option value="10">10</option>
+    <option value="20">20</option>
+  </select>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: String,
+});
+</script>
---
+++ 12-dars/src/components/base-textarea.vue
@@ -0,0 +1,27 @@
+<template>
+  <label v-if="desc" class="block mb-1" :for="desc">{{ label }}</label>
+  <textarea
+    class="w-full"
+    :id="desc"
+    :value="value"
+    :cols="cols"
+    :rows="rows"
+    :placeholder="placeholder"
+    @input="$event => $emit('update:modelValue', $event.target.value)"
+  ></textarea>
+</template>
+
+<script setup>
+const props = defineProps({
+  desc: {
+    type: String,
+    default: "",
+  },
+  label: String,
+  placeholder: String,
+  value: String,
+  cols: String,
+  modelValue: String,
+  rows: String,
+});
+</script>
