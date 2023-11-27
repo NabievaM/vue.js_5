67e0b09cec2d0105d703638ba1c9d11868b846af
NabievaM <101929794+NabievaM@users.noreply.github.com>
2023-11-28 00:17:00 +0500

    homework-13

---
+++ 13-homework/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const products = ref(null);
+async function getProducts() {
+  const res = await fetch("https://dummyjson.com/products");
+  products.value = await res.json();
+}
+onMounted(() => {
+  getProducts();
+});
+</script>
+
+<template>
+  <pre>{{ products }}</pre>
+</template>
+
+<style scoped></style>
