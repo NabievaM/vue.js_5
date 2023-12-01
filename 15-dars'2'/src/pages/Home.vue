f3c242a88a1da2726d150992c8c9442fce79ae42
NabievaM <101929794+NabievaM@users.noreply.github.com>
2023-12-02 00:45:39 +0500

    homework

--- 15-dars'2'/src/pages/Home.vue
+++ 15-dars'2'/src/pages/Home.vue
@@ -1,29 +1,39 @@
 <template>
-    <nav-bar :is-auth="isAuth" @logout="onLogout"/>
-    <!-- <KeepAlive> -->
-        <div class="flex" v-for="product of products" :key="product.id">
-            <ProductCard :product="product"/>
-        </div>
-    <!-- </KeepAlive> -->
+  <nav-bar :is-auth="isAuth" @logout="onLogout" />
+
+  <!-- <KeepAlive> -->
+  <div class=" text-slate-500 text-center text-2xl p-2 mt-1">
+    <h1>All Products</h1>
+  </div>
+  <div class="flex flex-wrap p-10">
+    <div
+      class="border-2 border-slate-300"
+      v-for="product of products"
+      :key="product.id"
+    >
+      <ProductCard :product="product" />
+    </div>
+  </div>
+
+  <!-- </KeepAlive> -->
 </template>
 
 <script setup>
-import { useHome } from '../composables/home';
-import { useLogin } from '../composables/login';
+import { useHome } from "../composables/home";
+import { useLogin } from "../composables/login";
 
-import ProductCard from '../components/ProductCard.vue'
-import NavBar from '../components/Navbar.vue';
+import ProductCard from "../components/ProductCard.vue";
+import NavBar from "../components/Navbar.vue";
 
-const { products, onLogout } = useHome()
-const { isAuth } = useLogin()
-
+const { products, onLogout } = useHome();
+const { isAuth } = useLogin();
 </script>
 
 <style>
 .flex {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
 }
 </style>
--- 15-dars'2'/src/pages/Login.vue
+++ 15-dars'2'/src/pages/Login.vue
@@ -1,21 +1,37 @@
 <template>
-    <div>
-        <div>
-            <input type="text" v-model="username"/>
-        </div>
-        <div>
-            <input type="password" v-model="password"/>
-        </div>
-        <button @click="onLogin">Login</button>
+  <div class="bg-green-300 text-center p-2">
+    <h1 class="text-white text-2xl">Login Page</h1>
+  </div>
+  <div
+    class="text-center mt-[150px] w-[600px] border-2 border-green-300 m-auto p-20"
+  >
+    <div class="mb-3">
+      <input
+        class="w-[90%] border-2 p-1"
+        type="text"
+        placeholder="Enter username"
+        v-model="username"
+      />
     </div>
+    <div>
+      <input
+        class="w-[90%] border-2 p-1 mb-3"
+        type="password"
+        placeholder="Enter password"
+        v-model="password"
+      />
+    </div>
+    <button
+      @click="onLogin"
+      class="w-[100px] bg-green-300 text-white rounded-lg p-3 text-xl"
+    >
+      Login
+    </button>
+  </div>
 </template>
 
 <script setup>
-import { useLogin } from '../composables/login';
+import { useLogin } from "../composables/login";
 
-const {
-    username,
-    password,
-    onLogin
-} = useLogin()
+const { username, password, onLogin } = useLogin();
 </script>
