88b434d512543c88dff5a7d1d1e7b2aec2d28d00
NabievaM <101929794+NabievaM@users.noreply.github.com>
2023-12-05 11:35:18 +0500

    backend for exam6

--- exam6-backend/src/app.module.ts
+++ exam6-backend/src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- exam6-backend/src/main.ts
+++ exam6-backend/src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const start = async () => {
-  try {
-    const app = await NestFactory.create(AppModule);
-    const PORT = process.env.PORT;
-    app.setGlobalPrefix('api');
-    app.listen(PORT, () => {
-      console.log(`Server is running on ${PORT} port`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-start();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
